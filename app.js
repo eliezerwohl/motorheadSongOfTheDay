97e45b46078536afcdd745fa5ca91a7d6d3b7574
Eliezer Wohl <eliezerwohl@gmail.com>
2017-01-19 19:18:35 -0500

    basic server, is now listening on port 9000

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-app.js
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 9000;
+// var mysql      = require('mysql');
+// var password = require("./ignore.js");
+// var table = require("./server/controllers/tableController.js")
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   port: 3306,
+//   password:password.password,
+//   database : 'motorheadDb'
+// });
+// connection.query("SHOW TABLES LIKE 'user';", function(err, rows, fields) {
+//   	if (err) throw err;
+//   	if (rows.length > 0) {
+//   		console.log("Db exists")
+//   	}
+//   	else{
+//   		table.createTable(connection);
+//   	}
+// });
+var logger = require("morgan");
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(express.static(__dirname + '/public'));
+// require('./routes')(app, connection);
+
+
+app.listen(PORT, function(){
+	console.log("listening on " + PORT)
+})
