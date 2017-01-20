2735a5660ab6f652f32cbe6d6730311b011164ad
Eliezer Wohl <eliezerwohl@gmail.com>
2017-01-19 19:25:24 -0500

    helloworld

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,20 @@
-index.js
+function sendFunc(res, string){
+	res.sendFile(process.cwd() + "/public/views/" + string +".html");
+}
+function queryFunc(connection, string,  callback){
+connection.query(string, function(err, rows, fields) {
+  	if (err) throw err;
+  	callback(rows)
+		});
+}
+
+module.exports = function(app,connection) {
+
+	app.get("/", function(req, res){
+		// res.send("got it")
+		sendFunc(res, "home");
+	});
+	app.get("*", function(req,res){
+			res.send("404'd!")
+		})
+}
